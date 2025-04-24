6a0f204afe7f49e2a258b7d4f8c58c8899db47f8
Aleksandr Zaugolnov <zausasha@gmail.com>
2025-04-24 20:56:23 +0300

    в метод main добавили код ввода чисел через консоль и код который вычисляет и выводит в консоль сумму, разность, произведение и частное этих чисел

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.print("Сумма чисел: ");
+        System.out.println(firstNumber+secondNumber);
+        System.out.print("Разность чисел: ");
+        System.out.println(firstNumber - secondNumber);
+        System.out.print("Произведение чисел: ");
+        System.out.println(firstNumber*secondNumber);
+        System.out.print("Частное чисел: ");
+        System.out.println((double)firstNumber/secondNumber);
+
     }
 }
