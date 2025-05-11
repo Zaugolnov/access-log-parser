4c1d42e09423157d9ce6b04bd15794e7af96dfc5
Aleksandr Zaugolnov <zausasha@gmail.com>
2025-05-11 11:19:35 +0300

    В метод main написал бесконечный цикл while, котором в консоли запрашиваться путь к файлу и проверяет его наличие

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int count = 0;
+        //c:/java/access.log
+        while (true) {
+            System.out.println(" Введите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            System.out.println(path);
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Файл не существует или указанный путь является путём к папке");
+                continue;
+            }
+            count++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + count);
+        }
     }
 }
